58f1e8f5f36efff9c47933974f5648fec438865a
amaotosarasa <amaotosarasa@gmail.com>
2022-10-04 23:07:21 +0900

    [modify] common.cssの読み込み

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import {Routes, Route} from 'react-router-dom';
 import {TopPage} from './page/TopPage';
+import {Header} from './component/Header';
+
+import './common/css/common.css';
 
 function App() {
   return (
     <div className="App">
+      <Header>サンプル</Header>
       <Routes>
         <Route path="/" element={<TopPage />} />
       </Routes>
